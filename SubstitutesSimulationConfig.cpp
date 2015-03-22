b1f3e38e69ada20bd765e187c477fde7909a4ccb
olya-d <inremajor@gmail.com>
2015-03-22 14:12:54 +0100

    fix bug in reading config

--- SubstitutesSimulationConfig.cpp
+++ SubstitutesSimulationConfig.cpp
@@ -12,16 +12,17 @@ SubstitutesSimulationConfig::SubstitutesSimulationConfig(std::string path_to_con
 
 void SubstitutesSimulationConfig::readConfig(std::string path) {
     Json::Value root;
-    std::ifstream config_doc("config.json", std::ifstream::binary);
+    std::ifstream config_doc(path, std::ifstream::binary);
     config_doc >> root;
 
     readCommonConfig(root);
 
     const Json::Value goods_from_config = root["goods"];
-    std::vector<std::string> goods = std::vector<std::string>();
-    std::map<std::string, int> max_starting_profits = std::map<std::string, int>();
-    std::map<std::string, int> max_acceptable_prices = std::map<std::string, int>();
-    std::map<std::string, int> costs = std::map<std::string, int>();
+    std::cout << goods_from_config;
+    goods = std::vector<std::string>();
+    max_starting_profits = std::map<std::string, int>();
+    max_acceptable_prices = std::map<std::string, int>();
+    costs = std::map<std::string, int>();
 
     for (int i = 0; i < goods_from_config.size(); ++i) {
         goods.push_back(goods_from_config[i].asString());
@@ -29,10 +30,6 @@ void SubstitutesSimulationConfig::readConfig(std::string path) {
         max_acceptable_prices[goods[i]] = root["max_acceptable_prices"][goods[i]].asInt();
         costs[goods[i]] = root["costs"][goods[i]].asInt();
     }
-    goods = std::vector<std::string> (goods);
-    max_starting_profits = std::map<std::string, int>(max_starting_profits);
-    max_acceptable_prices = std::map<std::string, int>(max_acceptable_prices);
-    costs = std::map<std::string, int>(costs);
 }
 
 
